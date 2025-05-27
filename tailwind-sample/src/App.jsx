c99499e7aa7792a51fc0bb7c176937e1fc12975a
Maktoumali <137157691+Maktoumali@users.noreply.github.com>
2025-05-27 23:22:41 +0530

    initial commit after making changes

---
+++ tailwind-sample/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './style.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+      <div className='bg-blue-700 h-15 flex items-center justify-evenly'>
+        <h1>Heading</h1>
+        <ul className='flex justify-evenly w-80 '>
+          <li className='transition delay-105 duration-300 ease hover:text-white cursor-pointer'>home</li>
+          <li className='nav-links'>home</li>
+          <li>home</li>
+          <li>home</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
