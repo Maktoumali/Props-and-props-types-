942d7da908887b985260771d2fce98ad46be6cb5
Maktoumali <137157691+Maktoumali@users.noreply.github.com>
2025-05-28 15:45:39 +0530

    just playing around with scale property

--- tailwind-sample/src/components/FunctionBased.jsx
+++ tailwind-sample/src/components/FunctionBased.jsx
@@ -16,7 +16,7 @@ const FunctionBased = () => {
         <div>
             {words && <div className='w-30 text-center bg-sky-300 transition-all delay-100 duration-150 ease-in'>{words}</div>}
             <button onClick={displayWord} className='w-20 bg-sky-400 h-10 rounded-xl'>click</button>
-            <div className='w-100 h-40 bg-gray-800 rounded-sm flex justify-evenly items-center'>
+            <div className='w-100 h-40 bg-gray-800 rounded-sm flex justify-evenly items-center transition duration-100  delay-10 ease-in hover:scale-120  '>
                 {display ? (
                     <div className='text-amber-50'>{display}</div>
                 ) : (
